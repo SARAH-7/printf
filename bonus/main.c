99b21823367b728b01902ae70e2ee7acb95bfb8a
SARAH-7 <62673148+SARAH-7@users.noreply.github.com>
2024-01-22 11:26:38 +0400

    Delete bonus/main.c

--- bonus/main.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-
-int	main(void)
-{
-	printf("\nft_printf return value: %d", ft_printf(" %d ", -1));
-	printf("\n");
-	printf("\nprintf return value: %d", printf(" %d ", -1));
-}
